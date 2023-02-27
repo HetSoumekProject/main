e6c54791b1f4fac1b3062a2dfba6f93605395e0b
seifeddine1598 <saifoujab@gmail.com>
2023-02-27 18:49:21 +0100

    seve

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react';
+import AllPosts from './components/AllPosts';
 
 function App() {
   return (
     <div className="App">
-    
+    <AllPosts/>
     </div>
   );
 }
