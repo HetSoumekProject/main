74563dfec4e0f0ec7fe0262026f10fa889a85e7a
mehdi_jaouedi <mehdijaouedi9@gmaile.com>
2023-03-03 10:41:53 +0100

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,35 @@
-import './App.css';
 
-// import Post from './components/Post.jsx';
-// import AllPosts from './components/AllPosts.jsx';
-// import ChatRoom from './components/ChatRoom';
-// import AllPending from './components/AllPending.jsx'
-// import WhatIs from './components/WhatIs';
-import NavBar from './components/NavBar';
-import Dashboard from './components/AdminDash';
-// import HelpPage from './components/HelpPage';
+import './App.css';
+import Post from './components/Post.jsx';
+import AllPosts from './components/AllPosts.jsx';
+import ChatRoom from './components/ChatRoom';
+import AllPending from './components/AllPending.jsx'
+import WhatIs from './components/WhatIs.jsx';
+import NavBar from './components/NavBar.jsx';
+import PrivatChat from './components/PrivatChat.jsx'
+import SuccessPayment from "./components/SuccessPayment.jsx"
+import FailPayment from './components/FailPayment.jsx';
+import Payment from './components/Payment.jsx';
+import Dashboard from './components/AdminDash.jsx';
+import { Route,Routes } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-     <Dashboard/>
-      {/* <WhatIs/> */}
-       {/* <HelpPage/> */}
-      {/* <ChatRoom/>
-    <AllPosts/>
-    <Post/>
-    <AllPending/> */}
+    <NavBar/>
+    <Routes>
+    {/* <Route path="/" exact  element={<Home/>}/> */}
+      <Route path="/AllPosts" element={<AllPosts/>}/>
+      <Route path="/ChatRoom" element={<ChatRoom/>}/>
+      <Route path="/WhatIs" element={<WhatIs/>}/>
+      <Route path="/AllPending" element={<AllPending/>}/>
+      <Route path="/Post" element={<Post/>}/>
+      <Route path="/PrivatChat" element={<PrivatChat/>}/>
+      <Route path="/Payment" element={<Payment/>}/>
+      <Route path="/FailPayment" element={<FailPayment/>}/>
+      <Route path="/SuccessPayment" element={<SuccessPayment/>}/>
+      <Route path="/AdminDashboard" element={<Dashboard/>}/>
+    </Routes>
     </div>
   );
 }
-
-export default App;
+export default App
