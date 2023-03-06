1385e5fc1648acc9bdde4c525f557b83ff972086
Sadmedous <117408605+Sadmedous@users.noreply.github.com>
2023-03-06 10:30:57 +0100

    updates and onecar

--- client/src/components/AllPosts.jsx
+++ client/src/components/AllPosts.jsx
@@ -1,11 +1,17 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 
-const AllPosts = ({car}) => {
+
+const AllPosts = ({car,setCar,setView,view}) => {
 
   return (
     <div>
-      <img className='imaget' style={{height:"200px",width:"300px"}} src={car.images[0].image}/>
+ <img className='imaget' style={{height:"200px",width:"300px"}} src={car.images[0].image} onClick={()=>{setView(!view)
+      setCar(car)
+      }}/>
+      <div>
+        
+      </div>
     </div>
   )
 }
