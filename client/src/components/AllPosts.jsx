e38819ea95655591cf689d6993d8b6ee64919073
seifeddine1598 <saifoujab@gmail.com>
2023-03-06 11:20:16 +0100

    css and some changes

--- client/src/components/AllPosts.jsx
+++ client/src/components/AllPosts.jsx
@@ -6,12 +6,7 @@ const AllPosts = ({car,setCar,setView,view}) => {
 
   return (
     <div>
- <img className='cars-pic'  src={car.images[0].image} onClick={()=>{setView(!view)
-      setCar(car)
-      }}/>
-      <div>
-        
-      </div>
+      <img className='cars-pic' src={car.images[0].image}/>
     </div>
   )
 }
